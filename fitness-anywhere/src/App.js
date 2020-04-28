248f2b7253711c5bf9d884bde7be9822b6c882eb
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-28 16:17:37 -0500

    axios with auth made

--- fitness-anywhere/src/App.js
+++ fitness-anywhere/src/App.js
@@ -2,44 +2,44 @@ import React from 'react';
 import './App.css';
 import { Route, Link, NavLink } from 'react-router-dom'
 import Form from './Component/form'
-import {Searchbar} from './Component/Searchbar'
-import {Login} from './Component/login'
-import {MySchedule} from './Component/mySchedule'
+import { Searchbar } from './Component/Searchbar'
+import { Login } from './Component/login'
+import { MySchedule } from './Component/mySchedule'
 
-function App() {
+function App()
+{
   return (
-    
+
     <div className="App">
       <div class="bg"></div>
       <nav>
-      <NavLink to="/">
-      Home
+        <NavLink to="/">
+          Home
       </NavLink>
-      <Link to="/login">
-      Login
+        <Link to="/login">
+          Login
       </Link>
-      <Link to="/signup">
-      Sign Up
+        <Link to="/signup">
+          Sign Up
       </Link>
-      <Link to="/myschedule">
-      My Schedule
-      {MySchedule}
+        <Link to="/myschedule">
+          My Schedule
       </Link>
-      <Link to="/clientschedule">
-      Client Schedule
-      </Link> 
+        <Link to="/clientschedule">
+          Client Schedule
+      </Link>
       </nav>
 
       <Searchbar></Searchbar>
 
-      <Route exact path ='/signup'>
+      <Route exact path='/signup'>
         <Form></Form>
       </Route>
-      <Route exact path ='/login'>
+      <Route exact path='/login'>
         <Login></Login>
       </Route>
       <Route exact path='/myschedule'>
-      {MySchedule}
+        <MySchedule />
       </Route>
     </div>
   );
