ba43ae9ef52cb116a278d4d3198e12c68a3046ea
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-29 16:36:35 -0500

    got instructors up

--- fitness-anywhere/src/Component/Instructors.js
+++ fitness-anywhere/src/Component/Instructors.js
@@ -2,17 +2,26 @@ import React, { useState, useEffect } from 'react';
 import * as yup from 'yup';
 import { axiosWithAuth } from '../Utils/axiosWithAuth';
 
-export const Instructors= () =>{
+export const Instructors = () =>
+{
 
+  const [instructors, setinstructors] = React.useState([])
+  React.useEffect(() =>
+  {
+    axiosWithAuth()
+      .get('/api/instructor')
+      .then(res =>{
+        console.log(res.data)
+        setinstructors(res.data)
+      })
+  }, [])
 
-  return(
+  return (
     <div>
-      
-      
-      Loading Instructors
-
-
-
+    <h1>instructors</h1>
+      {instructors.map(instructor=>(
+        <div>{instructor.username}</div>
+      ))}
 
     </div>
   )
