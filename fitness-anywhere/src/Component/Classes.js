6d7489d868e3d0cc334d5b2e4363507ef0d55ac0
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-29 17:15:26 -0500

    got id raised to function for deleting

--- fitness-anywhere/src/Component/Classes.js
+++ fitness-anywhere/src/Component/Classes.js
@@ -6,44 +6,54 @@ import { data } from '../Data/lorem'
 export const Classes = () =>
 {
   
-  const [classes,setClasses] = React.useState([])
-  useEffect(()=>{
+  const [classes, setClasses] = React.useState([])
+  useEffect(() =>
+  {
     axiosWithAuth()
     .get("/api/class")
-    .then(res =>{
-      setClasses(res.data)
-      console.log("classes", res.data)
-    })
-    .catch("not getting properly")
-  },[])
-
-const deleteMe = (e) =>{
-  e.preventDefault()
-}
-const edit = (e) =>{
-  e.preventDefault()
-  console.log(e)
-}
-
+    .then(res =>
+      {
+        setClasses(res.data)
+        console.log("classes", res.data)
+      })
+      .catch("not getting properly")
+    }, [])
+    
+    const edit = (e) =>
+    {
+      e.preventDefault()
+      console.log(e)
+    }
+    const deleteMe = (item) =>
+    {
+      // e.preventDefault()
+      axiosWithAuth()
+        .delete(`api/class/${item.id}`, item)
+        .then(res =>{
+          console.log(res)
+        })
+        .catch("")
+    }
+  
   // console.log("data", data)
   return (<div>
     {classes.map(item => (
-      <div class='card'>
-        <div class="item">Class Id: {item.id}</div>
-        <div class="item">Class Instructor: {item.instructor_id}</div>
-        <div class="item">Class Name: {item.name}</div>
-        <div class="item">Class Type: {item.type}</div>
-        <div class="item">Start Time: {item.start_time}</div>
-        <div class="item">Length: {item.duration}</div>
-        <div class="item">Level: {item.intensity}</div>
-        <div class="item">Location: {item.location}</div>
-        <div class="item">Attendees: {item.registered_attendees}</div>
-        <div class="item">Max Size: {item.max_size}</div>
-        <button onClick={deleteMe} id={item.id}> Delete</button>
+      <div key={item.id} className='card'>
+        <div className="item">Class Id: {item.id}</div>
+        <div className="item">Class Instructor: {item.instructor_id}</div>
+        <div className="item">Class Name: {item.name}</div>
+        <div className="item">Class Type: {item.type}</div>
+        <div className="item">Start Time: {item.start_time}</div>
+        <div className="item">Length: {item.duration}</div>
+        <div className="item">Level: {item.intensity}</div>
+        <div className="item">Location: {item.location}</div>
+        <div className="item">Attendees: {item.registered_attendees}</div>
+        <div className="item">Max Size: {item.max_size}</div>
+        <button onClick={()=>deleteMe(item)} > Delete</button>
         <button onClick={edit} id={item.id}>Edit</button>
       </div>
 
-    ))}
+))}
         Loading
 
   </div>
