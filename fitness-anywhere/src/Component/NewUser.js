6fffcd18b2073044d8ebb6ed096227bf169d5da8
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-29 12:26:05 -0500

    getting login form completed

--- fitness-anywhere/src/Component/NewUser.js
+++ fitness-anywhere/src/Component/NewUser.js
@@ -1,20 +1,25 @@
 import React from 'react'
-import axios from 'axios'
+import {axiosWithAuth} from '../Utils/axiosWithAuth'
+
+
 
 export const NewUser = () =>
 {
-
-
-  const [creds, setCreds] = React.useState({ name: "", password: "" })
+  const [creds, setCreds] = React.useState({ name: "", password: "", instructor:false })
 
   const handleClick = (e) =>
   {
     e.preventDefault()
+
+
+    axiosWithAuth
+    .post('/api/auth/client_register')
   }
 
   const onChange = (e) =>
   {
     setCreds({ ...creds, [e.target.name]: e.target.value })
+    console.log('state', creds)
   }
 
 
@@ -27,8 +32,11 @@ export const NewUser = () =>
       <form action="">
         <input onChange={onChange} type="text" label="name" name="name" placeholder="username" value={creds.name} />
         <input onChange={onChange} type="password" label="password" name="password" placeholder="Enter your password" value={creds.password} />
+        <label htmlFor="instructor">Instructor</label>
+
+        <input type="checkbox" value={creds.instructor} onChange={onChange}/>
         <br />
-        <button onClick={handleClick}>Enter</button>
+        <button onClick={handleClick} name='instructor'>Enter</button>
 
       </form>
 
