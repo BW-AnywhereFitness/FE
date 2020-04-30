6313e35bce822d45bad227231b91bff6740eb6cc
GeraldRyan <gerald.ryan40@yahoo.com>
2020-04-30 14:27:49 -0500

    getting form yup done

--- fitness-anywhere/src/Component/form.js
+++ fitness-anywhere/src/Component/form.js
@@ -17,10 +17,14 @@ const formSchema = yup.object().shape({
         .string(),
     intensity: yup
         .number(),
-    location: yup 
+    location: yup
         .string(),
     max_size: yup
-        .string()
+        .string(),
+    id: yup
+        .number(),
+    instructor_id: yup
+    .number()
 })
 
 
@@ -39,38 +43,68 @@ function Form()
         max_size: 0,
     }
 
-    
+
     const [errors, setErrors] = useState({
-        id: 0,
-        instructor_id: 0,
+        id: '',
+        instructor_id: '',
         name: "",
         type: "",
         start_time: "",
         duration: "",
-        intensity: 0,
+        intensity: '',
         location: "",
         register_attendees: "",
-        max_size: 0,
+        max_size: '',
     });
 
     const [values, setValues] = useState(initialFormValues)
 
-    function onInputChange(event)
+    function onInputChange(e)
     {
+        e.persist()
         setValues({
             ...values,
-            [event.target.name]: event.target.value
+            [e.target.name]: e.target.value
         })
+        console.log("Event from onChange", e)
+        validateChange(e)
         //console.log("event target name", [event.target.name], event.target.value)
         // console.log("values", values)
     }
     const [buttonDisabled, setButtonDisabled] = useState(true);
-    useEffect(() => {
-        formSchema.isValid(values).then(valid => {
+
+    useEffect(() =>
+    {
+        formSchema.isValid(values).then(valid =>
+        {
             setButtonDisabled(!valid);
         });
 
     }, [values]);
+
+    const validateChange = e =>
+    {
+        console.log("E from validate", [e.target.name])
+        yup
+            .reach(formSchema, e.target.name)
+            .validate(e.target.value)
+            .then(valid =>
+            {
+                setErrors({
+                    ...errors,
+                    [e.target.name]: ''
+                });
+
+            })
+            .catch(err =>
+            {
+                setErrors({
+                    ...errors,
+                    [e.target.name]: err.errors[0]
+                });
+            });
+    }
+
     const onSubmit = (e) =>
     {
         e.preventDefault()
@@ -104,6 +138,7 @@ function Form()
                     value={values.name}
                     onChange={onInputChange}
                 />
+                {errors.name.length > 0 ? (<p className="error">{errors.name}</p>) : null}
             </label>
 
             <label>
